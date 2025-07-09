684e7c33cc2df20559539e5c9989ae3c73aecc4f
usharma123 <125384326+usharma123@users.noreply.github.com>
2025-07-09 14:43:54 -0400

    updated meta-data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from '@/components/theme-provider'
 const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'John Doe',
+  title: 'Utsav Sharma',
 }
 
 export default function RootLayout({

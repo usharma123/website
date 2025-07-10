08a20ac31d38f88ee20acfdd9f3125d4427fb65d
usharma123 <125384326+usharma123@users.noreply.github.com>
2025-07-09 21:36:28 -0400

    fixed navbar sizing on mobile

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -29,13 +29,13 @@ export default function Nav() {
 
   return (
     <div className="fixed top-5 left-0 z-50 w-full flex justify-center">
-      <nav className="text-main-foreground border-border shadow-shadow rounded-base bg-main font-base flex w-max items-center gap-8 border-2 p-2.5 px-8 text-lg">
-        <div className="flex flex-1 items-center gap-8">
+      <nav className="text-main-foreground border-border shadow-shadow rounded-base bg-main font-base flex items-center max-w-2xl mx-auto gap-1 sm:gap-6 border-2 p-1.5 px-1 sm:p-2.5 sm:px-8 text-sm sm:text-base overflow-x-auto">
+        <div className="flex flex-1 items-center gap-1 sm:gap-6">
           {links.map((link) => (
             <Link
               key={link.path}
               className={clsx(
-                'hover:border-border rounded-base border-2 px-4 py-2 transition-colors',
+                'hover:border-border rounded-base border-2 px-2 py-1 sm:px-4 sm:py-2 transition-colors',
                 path === link.path ? 'border-border' : 'border-transparent',
               )}
               href={link.path}
@@ -44,7 +44,7 @@ export default function Nav() {
             </Link>
           ))}
         </div>
-        <div className="flex items-center justify-center ml-8">
+        <div className="flex items-center justify-center ml-1 sm:ml-6">
           <ThemeSwitcher />
         </div>
       </nav>
